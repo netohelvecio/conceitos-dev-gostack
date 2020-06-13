e78eaf3e04a52955450a3c93dc859c0bd5c038a7
netohelvecio <helvecioneto77@gmail.com>
2020-06-13 00:06:42 -0300

    feat: adding nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3333);
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoStack' })
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server is running in http://localhost:3333')
+});
